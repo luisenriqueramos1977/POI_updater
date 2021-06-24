9f2d0ebd94ba3398b066a2f9d5a3fe5c134747d0
Luis Ramos <luisenriqueramos1977@gmail.com>
2021-06-24 10:15:42 +0200

    status 10:15 24.06

--- POI_updater/src/fh_postdam/ramos/poiupdater/JenaUtilities.java
+++ POI_updater/src/fh_postdam/ramos/poiupdater/JenaUtilities.java
@@ -31,7 +31,6 @@ import org.apache.jena.util.iterator.ExtendedIterator;
 *
 */
 public class JenaUtilities {
-
 	/**
 	 * @param aDataset
 	 * @param aRoot
@@ -47,20 +46,130 @@ public class JenaUtilities {
 		 * reading every available dataset
 		 */
 		
-			//reading gleif database
+		//reading ontology dataset
 		
 		aDataset = TDB2Factory.connectDataset(aRoot);
-	    
 		aDataset.begin(ReadWrite.READ) ;
-		
 		//onto model of gleif1
-
 		aModel = aDataset.getDefaultModel() ;
-		
 		aOntModel = ModelFactory.createOntologyModel(aSpec, aModel);
-				
 		return aOntModel;
 		
+	}//ends readOntModelfromDataset
+	
+	/**
+	 * @param ns
+	 * @param IndClass
+	 * @param ontModel
+	 * @param ord
+	 * @return an Individual
+	 */
+	public static Individual getIndividualbyID(String ns, String IndClass, OntModel ontModel, String ID) {
+		OntClass  anIndClass;
+		Individual PoiInstance = null;
+		anIndClass = ontModel.getOntClass( ns + IndClass );
+		String currentID = "";
+		System.out.println("anIndClass  "+anIndClass);//flag
+		System.out.println("String id to be searched "+ID);//flag
+		//adapting ord
+		String ID2 = "\""+ID+"\"";
+		System.out.println("String ID modified "+ID2);//flag
+		 ExtendedIterator  model_items = anIndClass.listInstances();//here I get all instances in model
+			//begins loop 
+			 while (model_items.hasNext())
+		      {
+				 PoiInstance = (Individual) model_items.next();
+				 	try {
+				 		currentID = "";
+				 		currentID  = PoiInstance.getLabel("id").toString();
+				 			if ((ID.equalsIgnoreCase(currentID)) | (ID2.equalsIgnoreCase(currentID))){
+				 			System.out.println("------String found in jena utilities ---"+ID);//flag
+				 			System.out.println("------Found poi instance ---"+PoiInstance);//flag
+					 		return PoiInstance;
+					 	}	
+					 	else {
+					 		PoiInstance = null;
+					 	}
+				 	}
+				 	catch(java.lang.NullPointerException e) {
+				 		currentID= null;
+				 	}
+				 	if (!(currentID==null)) {
+				 		//we add quotation marks and a second option 
+				 }
+				 	
+		      }
+			 //GLEIF1_items.close();
+		return PoiInstance;
+	}//ends getIndividualbyID
+	
+	/**
+	 * @param ns
+	 * @param IndClass
+	 * @param ontModel
+	 * @param ord
+	 * @return an Individual
+	 */
+	public static Individual getIndividualbyName(String ns, String IndClass, OntModel ontModel, String Name) {
+		OntClass  anIndClass;
+		Individual PoiInstance = null;
+		anIndClass = ontModel.getOntClass( ns + IndClass );
+		String currentName = "";
+		System.out.println("anIndClass  "+anIndClass);//flag
+		System.out.println("String Name to be searched "+Name);//flag
+		//adapting ord
+		String Name2 = "\""+Name+"\"";
+		System.out.println("String ID modified "+Name2);//flag
+		 ExtendedIterator  model_items = anIndClass.listInstances();//here I get all instances in model
+			//begins loop 
+			 while (model_items.hasNext())
+		      {
+				 PoiInstance = (Individual) model_items.next();
+				 	try {
+				 		currentName = "";
+				 		currentName  = PoiInstance.getLabel("name").toString();
+				 			if ((Name.equalsIgnoreCase(currentName)) | (Name2.equalsIgnoreCase(currentName))){
+				 			System.out.println("------String found in jena utilities ---"+Name);//flag
+				 			System.out.println("------Found poi instance ---"+PoiInstance);//flag
+					 		return PoiInstance;
+					 	}	
+					 	else {
+					 		PoiInstance = null;
+					 	}
+				 	}
+				 	catch(java.lang.NullPointerException e) {
+				 		currentName= null;
+				 	}
+				 	if (!(currentName==null)) {
+				 		//we add quotation marks and a second option 
+				 }
+		      }
+			 //GLEIF1_items.close();
+		return PoiInstance;
+	}//ends getIndividualbyID
+
+	/**
+	 * @param aDataset
+	 * @param aRoot
+	 */
+	public static void closeReadOntModelfromDataset(Dataset aDataset, String aRoot) {
+		/*
+		 * reading every available dataset
+		 */
+		aDataset = TDB2Factory.connectDataset(aRoot);	    
+		aDataset.close();
+		
+	}//end of closeReadOntModelfromDataset
+
+	public static void closeWriteOntModelfromDataset(Dataset aDataset, String aRoot) {
+		/*
+		 * reading every available dataset
+		 */
+		aDataset = TDB2Factory.connectDataset(aRoot);		
+		aDataset.commit();
+		aDataset.close();
 	}
 
-}
+	
+
+}//JenaUtilities
