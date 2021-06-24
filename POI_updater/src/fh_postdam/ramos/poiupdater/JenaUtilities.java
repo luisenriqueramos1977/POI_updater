f3c9a0090b8a0d9abdafe55e3f5b1307116c812a
Luis Ramos <luisenriqueramos1977@gmail.com>
2021-06-24 08:54:41 +0200

    jena utilities

---
+++ POI_updater/src/fh_postdam/ramos/poiupdater/JenaUtilities.java
@@ -0,0 +1,14 @@
+package fh_postdam.ramos.poiupdater;
+/**
+*@author Luis Ramos luis.ramos@fh-potsdam.de
+*@version Creation time: 24.06.2021 08:53:52
+*Class Description:
+*this class contains specific methods which execute tasks that
+* can be considered repetitives along any programming related to 
+* jena API. 
+*
+*/
+public class JenaUtilities {
+
+	
+}
